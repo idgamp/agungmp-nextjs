81539f8fcd518ab1ef09437159b58aa99fb31edd
Agung Maha Putra <dev.agungmp@gmail.com>
2025-06-28 02:03:22 +0800

    feat: add remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  images: {
+    remotePatterns: [new URL("https://agungmp.com")],
+  },
 };
 
 export default nextConfig;
